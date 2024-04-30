5a5f06eab05567478730a5aacb87ed78e4209312
josh <21151212+joshuakan9@users.noreply.github.com>
2024-04-30 13:54:29 -0700

    changed dungeon to class, added a break condition

--- model/DungeonGenerator.js
+++ model/DungeonGenerator.js
@@ -1,64 +1,78 @@
-const rows = 5;
-const cols = 5;
-const initialRow = Math.floor(rows / 2);
-const initialCol = Math.floor(cols / 2);
-var dungeon = [];
+class DungeonGenerator {
 
-function createInitialDungeon() {
-    for (let i = 0; i < rows; i++) {
-        let row = [];
-        for (let j = 0; j < cols; j++) {
-            row.push('■');
-        }
-        dungeon.push(row);
+    constructor() {
+        const myRows = 5;
+        const myCols = 5;
+        const myInitialRow = Math.floor(myRows / 2);
+        const myInitialCol = Math.floor(myCols / 2);
+        var myDungeon = [];
     }
 
-    dungeon[initialRow][initialCol] = '□';
-    console.log(dungeon);
-}
-
-function isWithinBounds(theRow, col) {
-    return theRow >= 0 && theRow < rows && col >= 0 && col < cols;
-}
-
-function generate() {
-
-    while (dungeon[initialRow + 1][initialCol] === '■' || dungeon[initialRow - 1][initialCol] === '■' || dungeon[initialRow][initialCol + 1] === '■' || dungeon[initialRow][initialCol - 1] === '■') {
-        console.log('restarting from the intial position');
-        let totalRooms = 5;
-        let rowPos = Math.floor(rows / 2);
-        let colPos = Math.floor(cols / 2);
-
-        while (totalRooms > 0) {
-            let direction = Math.floor(Math.random() * 4);
-            let newRowPos = rowPos;
-            let newColPos = colPos;
-
-            if (direction === 0) {
-                newRowPos = rowPos - 1;
-                console.log('up');
-            } else if (direction === 1) {
-                newRowPos = rowPos + 1;
-                console.log('down');
-            } else if (direction === 2) {
-                newColPos = colPos + 1;
-                console.log('right');
-            } else if (direction === 3) {
-                newColPos = colPos - 1;
-                console.log('left');
+    createInitialDungeon() {
+        for (let i = 0; i < rows; i++) {
+            let row = [];
+            for (let j = 0; j < myCols; j++) {
+                row.push('■');
             }
+            this.myDungeon.push(row);
+        }
 
-            // check that the new room location is within bounds, and that it is not already a room
-            if (isWithinBounds(newRowPos, newColPos) && dungeon[newRowPos][newColPos] === '■') {
-                rowPos = newRowPos;
-                colPos = newColPos;
-                dungeon[rowPos][colPos] = '□';
-                totalRooms--;
-                console.log(dungeon);
+        this.myDungeon[this.myInitialRow][this.myInitialCol] = '□';
+        console.log(this.mythis.myDungeon);
+    }
+
+    isWithinBounds(theRow, col) {
+        return theRow >= 0 && theRow < this.myRows && col >= 0 && col < this.myCols;
+    }
+
+    generate() {
+        while (this.myDungeon[this.myInitialRow + 1][this.myInitialCol] === '■' ||
+            this.myDungeon[this.myInitialRow - 1][this.myInitialCol] === '■' ||
+            this.myDungeon[this.myInitialRow][this.myInitialCol + 1] === '■' ||
+            this.myDungeon[this.myInitialRow][this.myInitialCol - 1] === '■') {
+            console.log('restarting from the intial position');
+            let totalRooms = 5;
+            let rowPos = Math.floor(rows / 2);
+            let colPos = Math.floor(cols / 2);
+
+            while (totalRooms > 0) {
+                let direction = Math.floor(Math.random() * 4);
+                let newRowPos = rowPos;
+                let newColPos = colPos;
+                let directionCounter = 0;
+
+                if (direction === 0) {
+                    newRowPos = rowPos - 1;
+                    console.log('up');
+                    directionCounter++;
+                } else if (direction === 1) {
+                    newRowPos = rowPos + 1;
+                    console.log('down');
+                    directionCounter++;
+                } else if (direction === 2) {
+                    newColPos = colPos + 1;
+                    console.log('right');
+                    directionCounter++;
+                } else if (direction === 3) {
+                    newColPos = colPos - 1;
+                    console.log('left');
+                    directionCounter++;
+                }
+
+                // check that the new room location is within bounds, and that it is not already a room
+                if (isWithinBounds(newRowPos, newColPos) && this.myDungeon[newRowPos][newColPos] === '■') {
+                    rowPos = newRowPos;
+                    colPos = newColPos;
+                    this.myDungeon[rowPos][colPos] = '□';
+                    totalRooms--;
+                    console.log(this.myDungeon);
+                }
+
+                if (directionCounter >= 20) {
+                    console.log('no valid direction found, breaking out of the loop.')
+                    break;
+                }
             }
         }
     }
 }
-
-createInitialDungeon();
-generate();
