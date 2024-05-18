537d9e1d54d3b194658e665f8a85d939d55cd307
LeDuyhungC <hle4831@gmail.com>
2024-05-18 10:43:54 -0700

    transparent style

--- view/TransitionEffect.js
+++ view/TransitionEffect.js
@@ -8,19 +8,20 @@ class TransitionEffect {
     }
 
     transition() {
-        let backgroundColor = 150;
-        const rate = 25; // how fast the rendering is (based off of background color number line 22)
+        let alpha = 255;
+        let rate = 10; // how fast the rendering is (based off of background color number line 22)
         this.clear();
         const handler = () => { // anonymous function that is put into the setInterval
-            if (backgroundColor < 0) { // the break out case
+            if (alpha < 0) { // the break out case
                 this.clear();
                 return;
             }
             this.drawer = () => { // draws function draws effect
-                fill(backgroundColor, backgroundColor, backgroundColor);
+                fill(0, 0, 0, alpha);
                 rect(0, 0, width, height);
             }
-            backgroundColor -= rate;
+            alpha -= rate;
+            rate += rate * 2;
         }
         handler();
         this.interval = setInterval(handler, 100); // handler function and the time it takes for handler to be finished
